dbb7527eb62fea43a74a2664fad5803502507ac6
James Shawn Carnley <Shawn.Carnley@gatech.edu>
2025-08-01 21:28:43 -0400

    fix: update npm install command to use std_npm_args

--- Formula/it-tools-mcp.rb
+++ Formula/it-tools-mcp.rb
@@ -10,8 +10,8 @@ class ItToolsMcp < Formula
   depends_on "node"
 
   def install
-    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
-    bin.install_symlink Dir["#{libexec}/bin/*"]
+    system "npm", "install", *Language::Node.std_npm_args(libexec)
+    bin.install_symlink Dir[libexec/"bin/*"]
   end
 
   test do
